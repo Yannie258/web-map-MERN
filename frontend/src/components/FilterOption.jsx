31dca13e7439cd852f664320951f628e84f06f65
Yannie258 <yen_ngoc_289@yahoo.com>
2024-06-01 01:50:21 +0200

    redesign filter categories

--- frontend/src/components/FilterOption.jsx
+++ frontend/src/components/FilterOption.jsx
@@ -1,45 +1,68 @@
-import { Button } from '@material-tailwind/react';
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
+import Select from 'react-select';
 import { UserContext } from '../helpers/UserContext';
 
-// function FilterOption({ categories, selectedCategories, handleCategoryChange }) {
-function FilterOption({handleCategoryChange}) {
-  // @ts-ignore
+function FilterOption({ handleCategoryChange }) {
   const { categoriesColor } = useContext(UserContext);
-  console.log('categories from parent', categoriesColor);
-  // Assuming categoriesColor is an object mapping category names to color values
-  // Check if categoriesColor is undefined or null
-  if (!categoriesColor) {
-    // Render loading state or return null
-    return <div>Loading...</div>; // Or any loading state component
-  }
-  // Generate buttons based on categoriesColor
-  const categoryButtons = Object.entries(categoriesColor).map(([category, color]) => (
-    <Button
-      key={category}
-      size="sm"
-      className="capitalize"
-          style={{ backgroundColor: color, color: '#FFFFFF' }}
-          onClick={() => handleCategoryChange(category)}
-    >
-      {category}
-      </Button>
-      
-  ));
 
-    return (
-      <div className="flex justify-around space-x-2 ">
-        {categoryButtons}
-        <Button
-          size="sm"
-          className="capitalize"
-          style={{ backgroundColor: '#000000', color: '#FFFFFF' }}
-          onClick={() => handleCategoryChange(null)}
-        >
-          All
-        </Button>
-      </div>
-    );
+  if (!categoriesColor) {
+    return <div>Loading...</div>;
+  }
+
+  const dot = color => ({
+    alignItems: 'center',
+    display: 'flex',
+
+    ':before': {
+      backgroundColor: color,
+      borderRadius: 10,
+      content: '" "',
+      display: 'block',
+      marginRight: 8,
+      height: 10,
+      width: 10
+    }
+  });
+
+  const options = [
+    { value: '', label: 'All', color: '#000000' },
+    ...Object.entries(categoriesColor).map(([category, color]) => ({
+      value: category,
+      label: category,
+      color: color
+    }))
+  ];
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      ...dot(state.data.color),
+      padding: 10
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      ...dot(state.data.color)
+    }),
+    control: provided => ({
+      ...provided,
+      width: 250 // Set the desired width here
+    })
+  };
+
+  const handleChange = selectedOption => {
+    handleCategoryChange(selectedOption.value);
+  };
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <Select
+        defaultValue={options[0]}
+        options={options}
+        styles={customStyles}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
 
-export default FilterOption
+export default FilterOption;

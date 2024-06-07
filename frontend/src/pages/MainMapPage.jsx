6f06987ae04a128eadd647726c91dbc0f87e1153
Yannie258 <yen_ngoc_289@yahoo.com>
2024-06-07 19:38:36 +0200

    separate popup config to helper

--- frontend/src/pages/MainMapPage.jsx
+++ frontend/src/pages/MainMapPage.jsx
@@ -7,6 +7,7 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import FilterOption from '../components/FilterOption';
 import SearchWidget from '../components/SearchWidget';
 import { UserContext } from '../helpers/UserContext';
+import { createPopupContent } from '../helpers/PopUpConfig'; 
 
 function Map() {
   const mapRef = useRef(null);
@@ -35,87 +36,6 @@ function Map() {
       ...prevUser,
       homeAddress: null
     }));
-  };
-
-  const createPopupContent = category => {
-    const popupContent = document.createElement('div');
-    popupContent.innerHTML = `
-    <ul>
-      <li><b>Category:</b> ${category.name}</li>
-      <li><b>Name:</b> ${category.properties.BEZEICHNUNG}</li>
-      <li><b>Carry on:</b> ${category.properties.TRAEGER}</li>
-      <li><b>Address:</b> ${category.properties.STRASSE}</li>
-      <li><span>${category.properties.PLZ} ${category.properties.ORT}</span></li>
-      <li><b>Contact:</b> ${category.properties.TELEFON}</li>
-      <li><b>Web:</b> ${category.properties?.WWW}</li>
-      <li><b>Email:</b> ${category.properties?.EMAIL}</li>
-      <li><b>Longitude:</b> ${category.geometry.coordinates[0]}</li>
-      <li><b>Latitude:</b> ${category.geometry.coordinates[1]}</li>
-
-    </ul>
-
-    <button id='add-button'>Add favourite</button>
-    <button id='more-info'>More about this place</button>
-    <div id="additional-info">
-      
-    </div>
-  `;
-    // Add event listener for the button
-    const addButton = popupContent.querySelector('#add-button');
-    addButton.addEventListener('click', async () => {
-      // Handle removing the favorite here
-      console.log('add favorite clicked', category);
-      // Call your remove function here, e.g., removeFavorite(categoryId);
-      try {
-        const res = await axios.put(`/users/account/edit/${user._id}`, {
-          favourite: {
-            category: category.name,
-            //XY - Longitude-0, Latitude-1
-            favouriteLongitude: category.geometry.coordinates[0], //|
-            favouriteLatitude: category.geometry.coordinates[1]
-          }
-        });
-        // console.log('result: ', res);
-        alert('add home successfully');
-        window.location.href = '/';
-      } catch (error) {
-        console.error(error);
-      }
-    });
-
-    const getMoreInfos = popupContent.querySelector('#more-info');
-    //Send request to Nominatim API
-    getMoreInfos.addEventListener('click', async () => {
-      try {
-        console.log('cord', category.geometry.coordinates[0], category.geometry.coordinates[1]);
-        const res = await axios.post('/location-info-more', {
-          lat: category.geometry.coordinates[1],
-          lon: category.geometry.coordinates[0]
-        });
-        console.log('nominatim', res.data.extratags);
-
-        // Dynamically update the additional info section
-        const additionalInfo = popupContent.querySelector('#additional-info');
-        if (res.data.extratags) {
-          additionalInfo.innerHTML = `
-        <h3 id="more_header">Additional Information</h3>
-        <ul>
-          <li><b>Type:</b> ${res.data.type}</li>
-          <li><b>Display Name:</b> ${res.data.display_name}</li>
-          <li><b>Email:</b> ${res.data.extratags.email}</li>
-          <li><b>Tel:</b> ${res.data.extratags.phone}</li>      
-          <li><b>Lincence:</b> ${res.data.licence}</li> 
-        </ul>     
-      `;
-        } else {
-          additionalInfo.innerHTML = '<p>No additional data available.</p>';
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    });
-
-    return popupContent;
   };
 
   useEffect(() => {
@@ -161,7 +81,7 @@ function Map() {
             popupTemplate: {
               title: category.name, //category name
               // @ts-ignore
-              content: createPopupContent(category)
+              content: createPopupContent(category,user) // pass user context
             }
           });
           // more info to annothrt providers:
